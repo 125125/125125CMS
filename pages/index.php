99f0443ba8d4ed4fd54b92acac51f6c0c9559932
125125 <tim-levinsson@hotmail.com>
2014-09-11 23:32:18 +0200

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -3,14 +3,7 @@
           <center>News</center>
         </div>
         <div class="message">
-		<?php 
-		if ($_SESSION['username'] == "")
-			{
-				echo "Hello, <b>" . $_SESSION['username'] . "</b>";
-			}else{
-			
-			}
-		?>
+	<?php echo "Hello, <b>" . $_SESSION['username'] . "</b>" ?>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non augue eu tortor porttitor tempor nec sed dolor. Sed dui nulla, commodo eu pretium in, scelerisque quis dolor. In tempus justo id nulla dapibus interdum. Aenean a ante velit, nec rutrum arcu. Nullam mi nulla, consectetur ut gravida eget, ullamcorper nec magna. Proin sed mattis massa. Pellentesque neque mi, gravida id ornare vel, hendrerit et urna
         <br>
       </div>
