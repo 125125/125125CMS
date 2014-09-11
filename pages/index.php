b4c47ab4e55e06c6e755301a9ec78394e200bc60
125125 <tim-levinsson@hotmail.com>
2014-09-12 00:03:21 +0200

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,6 @@
           <center>News</center>
         </div>
         <div class="message">
-	<?php echo "Hello, <b>" . $_SESSION['username'] . "</b>" ?>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non augue eu tortor porttitor tempor nec sed dolor. Sed dui nulla, commodo eu pretium in, scelerisque quis dolor. In tempus justo id nulla dapibus interdum. Aenean a ante velit, nec rutrum arcu. Nullam mi nulla, consectetur ut gravida eget, ullamcorper nec magna. Proin sed mattis massa. Pellentesque neque mi, gravida id ornare vel, hendrerit et urna
         <br>
       </div>
