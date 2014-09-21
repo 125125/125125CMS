3bda3913b69b030b0ab8e08c2b0de2a39d281810
PhentomPT <carloslpmpc@gmail.com>
2014-09-21 01:59:50 +0100

    Vote Fix
    - File check_vote.php fixed.

--- content/pages/vote/check_vote.php
+++ content/pages/vote/check_vote.php
@@ -10,41 +10,45 @@ if (isset($_SESSION['username']) AND !empty($_SESSION['username'])){
 	
 	$last_time = $mysqli -> query("SELECT * FROM voted_cooldown WHERE username = 'admin'");
 	
-	while($row = $last_time -> fetch_array(MYSQLI_ASSOC)){
-		$last_time_voted = date("Y/m/d H:i:s", strtotime($row['voted_time']));
-		$id = $row['id'];
-	}
-	
 	$now = date("Y/m/d H:i:s", time());
 	
-	$can_vote = date("Y/m/d H:i:s",strtotime("+12 Hours", strtotime($last_time_voted)));
-	
-	$time_to_vote = date("H:i:s",strtotime($can_vote) - strtotime($now));
-	
-	//echo "Now: ". $now ."<p/>";
-	//echo "Last time: ". $last_time_voted ."<p/>";
-	//echo "Can Vote: ". $can_vote ."<p/>";
-	//echo "Try to vote in: ". $time_to_vote ." hours<p/>";
-	
-	//Debug to vote
-	//$can_vote = $now;
-	
-	$user =  $_SESSION['username'];
-	
-	if ($now >= $can_vote){
-		$mysqli -> query("UPDATE voted_cooldown SET voted_time = '$now' WHERE id = '$id'");
+	if ($last_time -> num_rows == 0){
+		$mysqli -> query("INSERT INTO voted_cooldown (username,voted_link,voted_time) VALUES('".$_SESSION['username']."','openwow','$now')");
 		include_once "openwow.php";
 	}
-	else {
-		echo '<div class="title">
-			<center>Permission Denied</center>
-		</div>
-		<div class="message">
-			<center>
-				<p>Sorry but you cannot vote yet</p>
-				Try vote again in '.$time_to_vote.' hours
-			</center>
-		</div>';
+	else{
+		while($row = $last_time -> fetch_array(MYSQLI_ASSOC)){
+			$last_time_voted = date("Y/m/d H:i:s", strtotime($row['voted_time']));
+			$id = $row['id'];
+		}
+		
+		$can_vote = date("Y/m/d H:i:s",strtotime("+12 Hours", strtotime($last_time_voted)));
+		
+		$time_to_vote = date("H:i:s",strtotime($can_vote) - strtotime($now));
+		
+		//echo "Now: ". $now ."<p/>";
+		//echo "Last time: ". $last_time_voted ."<p/>";
+		//echo "Can Vote: ". $can_vote ."<p/>";
+		//echo "Try to vote in: ". $time_to_vote ." hours<p/>";
+		
+		//Debug to vote
+		//$can_vote = $now;
+		
+		if ($now >= $can_vote){
+			$mysqli -> query("UPDATE voted_cooldown SET voted_time = '$now' WHERE id = '$id'");
+			include_once "openwow.php";
+		}
+		else {
+			echo '<div class="title">
+				<center>Permission Denied</center>
+			</div>
+			<div class="message">
+				<center>
+					<p>Sorry but you cannot vote yet</p>
+					Try vote again in '.$time_to_vote.' hours
+				</center>
+			</div>';
+		}
 	}
 }
 else{
